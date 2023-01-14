ce418db041f3bfd35ed1dd323e1c9d8966058cce
Jacek Szulc <pa23143@student.wsb.gdynia.pl>
2023-01-14 20:39:19 +0100

    Podstawowa wersja aplikacji Spring Boot

---
+++ src/main/java/com/example/projektpokazowyjs2023/ProjektPokazowyJs2023Application.java
@@ -0,0 +1,13 @@
+package com.example.projektpokazowyjs2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektPokazowyJs2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektPokazowyJs2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projektpokazowyjs2023/ProjektPokazowyJs2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektpokazowyjs2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektPokazowyJs2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
