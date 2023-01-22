64b97716640d1083296ab73d86c6019bdfd7b3ac
Jacek Szulc <pa23143@student.wsb.gdynia.pl>
2023-01-22 20:54:56 +0100

    Obsługa usuwania projektu #8

--- src/main/java/com/example/projektpokazowyjs2023/projects/Project.java
+++ src/main/java/com/example/projektpokazowyjs2023/projects/Project.java
@@ -3,6 +3,8 @@ package com.example.projektpokazowyjs2023.projects;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data // zastępuje kilka adnotacji jedną np. Getter, Setter
 @Entity // adnotacja: hibernate automatycznie utworzy tabelkę Project
 public class Project {
@@ -16,6 +18,8 @@ public class Project {
     private String code;
 
     private String description;
+
+    private Date dateCreated = new Date();
 
     private Boolean enabled = true;
 
