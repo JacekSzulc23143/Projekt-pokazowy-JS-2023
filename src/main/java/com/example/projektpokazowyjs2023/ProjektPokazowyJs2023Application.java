75a8af97988519028a136990454835c964e42405
Jacek Szulc <j.szulc77@wp.pl>
2023-05-14 20:58:44 +0200

    Zewnętrzna konfiguracja #56

--- src/main/java/com/example/projektpokazowyjs2023/ProjektPokazowyJs2023Application.java
+++ src/main/java/com/example/projektpokazowyjs2023/ProjektPokazowyJs2023Application.java
@@ -2,8 +2,10 @@ package com.example.projektpokazowyjs2023;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class ProjektPokazowyJs2023Application {
 
     public static void main(String[] args) {
