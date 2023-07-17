b9f5d1cd9fcc92e45ddbc310d2187d016fa99bf1
Jacek Szulc <j.szulc77@wp.pl>
2023-07-17 14:32:24 +0200

    AJAX - Priorytet #101

--- src/main/java/com/example/projektpokazowyjs2023/issues/IssuePriority.java
+++ src/main/java/com/example/projektpokazowyjs2023/issues/IssuePriority.java
@@ -1,5 +1,14 @@
 package com.example.projektpokazowyjs2023.issues;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum IssuePriority {
-    EASY, MEDIUM, SERIOUS
+    EASY, MEDIUM, SERIOUS;
+
+    // metoda która ze stringa robi enuma
+    @JsonCreator
+    public static IssuePriority fromString(@JsonProperty("priority") String value) {
+        return IssuePriority.valueOf(value);
+    }
 }
